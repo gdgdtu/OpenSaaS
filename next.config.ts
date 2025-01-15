fbaf9269f14450a24e108cd747fa7b40f64fb3c2
Arjit Saxena <arjit05saxena@gmail.com>
2025-01-15 19:28:50 +0530

    Added Clerk Authentication

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['img.clerk.com'], // Allow images from Clerk
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
